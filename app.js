52b49240595ff8ad072c6f314360463c9b52a5fa
marineco <marinecolonge@gmail.com>
2016-10-11 22:38:17 +0200

    Nettoyage de mon code Retirer les console.log / enlever les instructions commentées

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-// good luck !
-
-/* # Color Pairing
-
-## 1ere étape 
-Les boutons doivent avoir leur fond de la couleur qui se trouve dans le
-data attribute data-color */
 
 var content = $('input');
-
 var len = content.length;
 
 
@@ -18,10 +10,9 @@ for (i = 0; i < len; i++){
 }
 
 $('.color').click(function(){
+	
 	var couleur = $(this).data('color');
-
 	var fond = test();
-	console.log(fond);
 
 	if (fond){
 		$('.main').css("color", couleur);
@@ -34,14 +25,3 @@ $('.color').click(function(){
 function test(){
 	return $('#modify-texte').is(':checked');
 }
-
-
-
-
-/*## 2e étape 
-Un click sur le bouton doit mettre cette couleur en fond*/
-
-
-/*## 3e étape
-quand la case #modify-texte est cochée, la couleur du texte
-doit etre modifiée */
